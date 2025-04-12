589b2598211f7360fdcc99225ba98688fe177064
faroukazizi1 <azizi.farouk@esprit.tn>
2025-04-12 14:43:17 +0100

    Creer l'interface et le controller du login fonctionnelle

--- src/Controller/GestionUser/BackOffice/UserController.php
+++ src/Controller/GestionUser/BackOffice/UserController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
@@ -34,13 +35,14 @@ final class UserController extends AbstractController
     }
 
     #[Route('/home_back/GestionUser/Ajout', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager ,  UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+    
             $entityManager->persist($user);
             $entityManager->flush();
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,10 +7,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 
 use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: 'user')]
-class User
+class User implements UserInterface , PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -70,7 +72,7 @@ class User
         return $this;
     }
 
-    #[ORM\Column(type: 'string', nullable: false)]
+    #[ORM\Column(type: 'string', nullable: false , unique: true)]
     private ?string $email = null;
 
     public function getEmail(): ?string
@@ -199,6 +201,29 @@ class User
     {
         $this->getPromotions()->removeElement($promotion);
         return $this;
+    }
+
+    public function getRoles(): array
+    {   
+        $role = [];
+        if($this->role == 'Employe'){
+            $role[] = 'ROLE_Employe';
+        }
+        else if($this->role == 'RHR'){
+            $role[] = 'RHR';
+        }
+
+        return array_unique($role);
+    }
+
+    public function eraseCredentials() : void
+    {
+        
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->id;
     }
 
 }
