02ea6b62e149804aa0b8ab629e6a7324429d5e46
dorra <dorra.meddeb@esprit.tn>
2025-04-25 10:02:22 +0100

    calendrier

--- src/Controller/GestionFinance/PretController.php
+++ src/Controller/GestionFinance/PretController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Twilio\Rest\Client;
 
@@ -34,29 +35,26 @@ final class PretController extends AbstractController
             $entityManager->persist($pret);
             $entityManager->flush();
 
-            $sid = ''; // Ton SID Twilio
-            $authToken = ''; // Ton Auth Token Twilio
-            $fromNumber = ''; // Ton numéro Twilio
-            $toNumber = ''; // Numéro du destinataire
+            // Envoi de SMS
+            $sid = 'ACf47cce92a0f6a9ff6eb6b88ff7c797c8';
+            $authToken = '8c6b09a8ee5e0796002feb627f2ffb0d';
+            $fromNumber = '+14432780246';
+            $toNumber = '+21650856254';
             $client = new Client($sid, $authToken);
 
-            // Message personnalisé avec le numéro de CIN
-            $message = "Votre demande avec le CIN " . $pret->getCin() . " est prise en considération.";
+         $message = "Votre demande avec le CIN " . $pret->getCin() . " est prise en considération.";
 
             try {
-                $client->messages->create(
-                    $toNumber,
-                    [
-                        'from' => $fromNumber,
-                        'body' => $message,
-                    ]
-                );
+                $client->messages->create($toNumber, [
+                    'from' => $fromNumber,
+                    'body' => $message,
+                ]);
                 $this->addFlash('success', "Le prêt a été ajouté et un SMS de confirmation a été envoyé.");
             } catch (\Exception $e) {
                 $this->addFlash('warning', "Le prêt a été ajouté, mais l'envoi du SMS a échoué.");
             }
 
-            return $this->redirectToRoute('app_pret_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_pret_index');
         }
 
         return $this->render('GestionFinance/pret/new.html.twig', [
@@ -65,24 +63,35 @@ final class PretController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_pret_show', methods: ['GET'])]
-    public function show(Pret $pret): Response
+    #[Route('/{id<\d+>}', name: 'app_pret_show', methods: ['GET'])]
+    public function show(int $id, PretRepository $pretRepository): Response
     {
+        $pret = $pretRepository->find($id);
+
+        if (!$pret) {
+            throw $this->createNotFoundException('Prêt non trouvé.');
+        }
+
         return $this->render('GestionFinance/pret/show.html.twig', [
             'pret' => $pret,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_pret_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Pret $pret, EntityManagerInterface $entityManager): Response
+    #[Route('/{id<\d+>}/edit', name: 'app_pret_edit', methods: ['GET', 'POST'])]
+    public function edit(int $id, Request $request, PretRepository $pretRepository, EntityManagerInterface $entityManager): Response
     {
+        $pret = $pretRepository->find($id);
+
+        if (!$pret) {
+            throw $this->createNotFoundException('Prêt non trouvé.');
+        }
+
         $form = $this->createForm(PretType::class, $pret);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
-
-            return $this->redirectToRoute('app_pret_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_pret_index');
         }
 
         return $this->render('GestionFinance/pret/edit.html.twig', [
@@ -91,13 +100,57 @@ final class PretController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_pret_delete', methods: ['POST'])]
-    public function delete(Request $request, Pret $pret, EntityManagerInterface $entityManager): Response
+    #[Route('/{id<\d+>}', name: 'app_pret_delete', methods: ['POST'])]
+    public function delete(int $id, Request $request, PretRepository $pretRepository, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete' . $pret->getId_pret(), $request->getPayload()->getString('_token'))) {
+        $pret = $pretRepository->find($id);
+
+        if (!$pret) {
+            throw $this->createNotFoundException('Prêt non trouvé.');
+        }
+
+        if ($this->isCsrfTokenValid('delete' . $pret->getIdPret(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($pret);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_pret_index', [], Response::HTTP_SEE_OTHER);
-    }}
+        return $this->redirectToRoute('app_pret_index');
+    }
+
+    #[Route('/calendar', name: 'pret_calendar', methods: ['GET'])]
+    public function calendar(): Response
+    {
+        return $this->render('GestionFinance/pret/calendar.html.twig');
+    }
+
+    #[Route('/api/prets', name: 'api_prets', methods: ['GET'])]
+    public function apiPrets(PretRepository $pretRepository): JsonResponse
+    {
+        $prets = $pretRepository->findAll();
+        $events = [];
+
+        foreach ($prets as $pret) {
+            $events[] = [
+                'title' => 'Revenus: ' . $pret->getRevenusBruts(),
+                'start' => $pret->getDatePret()->format('Y-m-d'),
+                'url' => $this->generateUrl('app_pret_show', ['id' => $pret->getId_pret()]),
+            ];
+        }
+
+        return $this->json($events);
+    }
+
+    #[Route('/details/{id<\d+>}', name: 'pret_details', methods: ['GET'])]
+    public function details(int $id, PretRepository $pretRepository): Response
+    {
+        $pret = $pretRepository->find($id);
+
+        if (!$pret) {
+            throw $this->createNotFoundException('Prêt non trouvé.');
+        }
+
+        return $this->render('GestionFinance/pret/details.html.twig', [
+            'pret' => $pret,
+        ]);
+    }
+}
