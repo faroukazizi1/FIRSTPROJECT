19e2aad6cf753c8367981fc8680a61786a3b7a7e
benghorbelmenyar <menyar.benghobel@esprit.tn>
2025-04-30 11:44:29 +0100

    test

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -12,60 +12,85 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Formateur;
 use App\Form\FormationSearchType;
+use App\Service\MailService;
 
 
 
 #[Route('/formation')]
 final class FormationController extends AbstractController
 {
-    #[Route(name: 'app_formation_index', methods: ['GET'])]
-public function index(Request $request, FormationRepository $formationRepository): Response
-{
-    $form = $this->createForm(FormationSearchType::class);
-    $form->handleRequest($request);
-
-    $formations = [];
-
-    if ($form->isSubmitted() && $form->isValid()) {
-        $data = $form->getData();
-        $titre = $data['titre'] ?? '';
-
-        $formations = $formationRepository->createQueryBuilder('f')
-            ->where('f.Titre LIKE :titre')
-            ->setParameter('titre', '%' . $titre . '%')
-            ->getQuery()
-            ->getResult();
-    } else {
-        $formations = $formationRepository->findAll();
+    // src/Controller/FormationController.php
+    #[Route('/', name: 'formation_index', methods: ['GET'])]
+    public function index(Request $request, FormationRepository $formationRepository): Response
+    {
+        $form = $this->createForm(FormationSearchType::class);
+        $form->handleRequest($request);
+    
+        // Debug: afficher les données du formulaire
+        if ($form->isSubmitted()) {
+            dump($form->getData());
+        }
+    
+        $formations = $formationRepository->searchAndSort(
+            $form->isSubmitted() ? $form->getData() : [],
+            $request->query->get('sort')
+        );
+    
+        // Debug: afficher les résultats
+        dump($formations);
+    
+        return $this->render('formation/index.html.twig', [
+            'formations' => $formations,
+            'form' => $form->createView()
+        ]);
     }
 
-    return $this->render('formation/index.html.twig', [
-        'formations' => $formations,
-        'form' => $form->createView()
+// Nouvelle route pour les statistiques
+#[Route('/statistiques', name: 'app_formation_stats', methods: ['GET'])]
+public function statistics(FormationRepository $formationRepository): Response
+{
+    $stats = [
+        'total_formations' => $formationRepository->count([]),
+        'formations_par_type' => $formationRepository->countByType(),
+        'duree_moyenne' => $formationRepository->averageDuration(),
+        'prochaines_formations' => $formationRepository->findUpcoming(5)
+    ];
+
+    return $this->render('formation/statistics.html.twig', [
+        'stats' => $stats
     ]);
 }
 
 
 
-    #[Route('/new', name: 'app_formation_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $formation = new Formation();
-        $form = $this->createForm(FormationType::class, $formation);
-        $form->handleRequest($request);
+#[Route('/formation/new', name: 'app_formation_new')]
+public function new(Request $request, EntityManagerInterface $em, MailService $mailService): Response
+{
+    $formation = new Formation();
+    $form = $this->createForm(FormationType::class, $formation);
+    $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($formation);
-            $entityManager->flush();
+    if ($form->isSubmitted() && $form->isValid()) {
+        $em->persist($formation);
+        $em->flush();
 
-            return $this->redirectToRoute('app_formation_index', [], Response::HTTP_SEE_OTHER);
-        }
+        // Envoi de mail au formateur
+        $formateur = $formation->getFormateur();
+        $mailService->envoyerMail(
+            $formateur->getEmail(),
+            'Nouvelle formation assignée',
+            "Bonjour " . $formateur->getNomF() . ",\n\nVous êtes assigné à la formation : " . $formation->getTitre()
+        );
 
-        return $this->render('formation/new.html.twig', [
-            'formation' => $formation,
-            'form' => $form,
-        ]);
+        $this->addFlash('success', 'Formation ajoutée et mail envoyé.');
+
+        return $this->redirectToRoute('formation_index');
     }
+
+    return $this->render('formation/new.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}
 
     #[Route('/{id_form}', name: 'app_formation_show', methods: ['GET'])]
     public function show(Formation $formation): Response
@@ -84,7 +109,7 @@ public function index(Request $request, FormationRepository $formationRepository
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_formation_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('formation_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('formation/edit.html.twig', [
@@ -101,10 +126,49 @@ public function index(Request $request, FormationRepository $formationRepository
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_formation_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('formation_index', [], Response::HTTP_SEE_OTHER);
     }
    
 
-  
+    #[Route('/formation/validation-test', name: 'formation_validation_test')]
+    public function validationTest(Request $request): Response
+    {
+        $form = $this->createForm(ValidationTestType::class);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $formationType = $data['formationType'];
+            $question1 = $form->get('question1')->getData();
+            $question2 = $form->get('question2')->getData();
+    
+            $score = 0;
+    
+            if ($formationType === 'softskills') {
+                if ($question1 === 'b') $score++;
+                if ($question2 === 'c') $score++;
+            } elseif ($formationType === 'web') {
+                if ($question1 === 'a') $score++;
+                if ($question2 === 'b') $score++;
+            } elseif ($formationType === 'mobile') {
+                if ($question1 === 'c') $score++;
+                if ($question2 === 'a') $score++;
+            }
+    
+            $pourcentage = ($score / 2) * 100;
+    
+            if ($pourcentage >= 70) {
+                $this->addFlash('success', 'Bravo ! Vous avez réussi le test.');
+            } else {
+                $this->addFlash('danger', 'Désolé, vous n\'avez pas validé.');
+            }
+    
+            return $this->redirectToRoute('formation_validation_test');
+        }
+    
+        return $this->render('formation/validation_test.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
     
 }
